a5d0516436684c94c18474de80f176f692504101
ibenross <34763956+ibenross@users.noreply.github.com>
2023-12-08 13:59:18 -0500

    Add files via upload
    Updates Day 2

--- background.js
+++ background.js
@@ -1,15 +1,29 @@
 const acronymDictionary = {
-    // Add your acronyms and their definitions here
     "HTML": "Hypertext Markup Language",
     "CSS": "Cascading Style Sheets",
     // Add more acronyms as needed
   };
   
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'checkAcronym') {
+  let connectedPort;
+  
+  chrome.runtime.onConnect.addListener((port) => {
+    if (port.name === 'content-script') {
+      connectedPort = port;
+      port.onDisconnect.addListener(() => {
+        connectedPort = null;
+      });
+    }
+  });
+  
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'checkAcronym' && connectedPort) {
       const acronym = request.acronym.toUpperCase();
       const definition = acronymDictionary[acronym];
-      sendResponse(definition);
+      connectedPort.postMessage({ action: 'acronymCheckResponse', definition: definition });
+    } else if (request.action === 'showTooltip') {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'showTooltip', acronym: request.acronym, definition: request.definition, position: request.position });
+      });
     } else if (request.action === 'getAcronymInfo') {
       // Additional logic for getting information from the page if needed
       sendResponse(null);
